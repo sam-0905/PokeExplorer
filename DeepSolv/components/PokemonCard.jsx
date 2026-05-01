f427d1d1685cacedee1c9f0e61759c148c79d28e
Allan sam <allansam0905@gmail.com>
2026-05-01 22:47:41 +0530

    fix - Removed extra fetching bcz we implemented inside the home page itself , so we just directly used

--- DeepSolv/components/PokemonCard.jsx
+++ DeepSolv/components/PokemonCard.jsx
@@ -5,28 +5,13 @@ import { useEffect, useState } from "react";
 
 const PokemonCard = ({data}) =>{
 
-    const [image, setImage] = useState(null);
     const navigate = useNavigate();
-
-    useEffect(() => {
-        const fetchImg = async () => {
-            try{
-                const res = await fetch(data.url)
-                const imgData = await res.json();
-                setImage(imgData.sprites.front_default);
-            }
-            catch(err){
-                console.log(err);
-            }
-        }
-        fetchImg();
-    }, [data.url])
 
     return(
         <>
         <div className="card">
             <h3 className="card-name">{data.name}</h3>
-            {image && <img src={image} alt={data.name} className="card-img" />}
+            <img src={data.image} alt={data.name} className="card-img" />
             <button className="details-btn" onClick={() => navigate(`/pokemon/${data.name}`)}>Info</button>
         </div>
         </>
