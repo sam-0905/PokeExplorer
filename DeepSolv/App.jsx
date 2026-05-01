bc71f7255b9c0b4a146edd79d1505b0e66e7ba7e
Allan sam <allansam0905@gmail.com>
2026-05-01 19:41:20 +0530

    Added a new route Pokemon details

--- DeepSolv/App.jsx
+++ DeepSolv/App.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
+import PokemonDetails from "./pages/PokemonDetails";
 
 const App = () => {
-
 
     return (
          <>
         <Routes>
             <Route path="/" element={<Home/>} />
+            <Route path="/pokemon/:name" element={<PokemonDetails/>} />
         </Routes>   
          </>
     )}
